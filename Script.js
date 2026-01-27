8c43ad715725a8be9576d7c48fffecfd30dc8034
hcchly <haifathefirst@gmail.com>
2026-01-27 07:53:49 +0300

    Initial commit

---
+++ Script.js
@@ -0,0 +1,24 @@
+// SIGN UP -> go to login page
+const signupForm = document.getElementById("signupForm");
+if (signupForm) {
+  document.getElementById("profileImg").setAttribute("accept", "image/*"); // images only
+
+  signupForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    window.location.href = "user.html";
+  });
+}
+
+// LOGIN USER -> go to user page
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    window.location.href = "user.html";
+  });
+
+  // LOGIN ADMIN -> go to admin page
+  document.getElementById("adminBtn").addEventListener("click", function () {
+    window.location.href = "admin.html";
+  });
+}
